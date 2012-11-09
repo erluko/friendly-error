5d1361013be57acb59dec6afc3f8ca0223a83097
Eric Kobrin <erluko+git@gmail.com>
2012-11-08 21:40:18 -0500

    better

--- src/friendly_error/core.clj
+++ src/friendly_error/core.clj
@@ -1,7 +1,6 @@
 (ns friendly-error.core)
 
-
-(defn friendly
+(defn friendly-forms
   [fun & checks]
   (prn [fun checks])
   (let [anames (map symbol
@@ -10,11 +9,15 @@
         achecks (vec (partition
                  2
                  (interleave checks anames)))]
-  (eval `(fn [~@anames]
-           {:pre ~achecks}
-           (~fun ~@anames)))))
+   `(fn [~@anames]
+            {:pre ~achecks}
+            (~fun ~@anames))))
 
-(defmacro make-friend
+(defn friendly-fn
+  [fun & checks]
+  (eval (apply friendly-forms fun checks)))
+
+(defmacro make-friend!
   [fun & checks]
   `(do
      (alter-var-root (var ~fun)
