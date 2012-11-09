b929c3d7efb65439e14ce43bf1ca60e1b4c69ace
Eric Kobrin <erluko+git@gmail.com>
2012-11-09 09:29:11 -0500

    unfriend returns enough to re-friend

--- src/friendly_error/core.clj
+++ src/friendly_error/core.clj
@@ -2,16 +2,18 @@
 
 (defn friendly-forms
   [fun & checks]
-  (let [anames (map symbol
-                    (map (partial str "arg")
-                         (range (count checks))))
+  (let [anames (map (comp symbol
+                          (partial str "arg"))
+                          (range (count checks)))
         achecks (vec (partition
                  2
                  (interleave checks anames)))]
     `(vary-meta (fn [~@anames]
                   {:pre ~achecks}
                   (~fun ~@anames))
-                assoc :friend ~fun)))
+                assoc
+                :friend {:fun ~fun
+                         :checks (list ~@checks)})))
 
 (defn friendly-fn
   [fun & checks]
@@ -27,7 +29,8 @@
 (defmacro un-friend! [fun]
   `(when-let [friend# (:friend (meta ~fun))]
      (alter-var-root (var ~fun)
-                     (constantly friend#))))
+                     (constantly (:fun friend#)))
+     friend#))
 
 ;;(friendly map is-function? is-collection?)
 ;;
