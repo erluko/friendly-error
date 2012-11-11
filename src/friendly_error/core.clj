cc692755520436998ef4b48facb686affa6b93f8
Eric Kobrin <erluko+git@gmail.com>
2012-11-11 10:25:07 -0500

    docstrings, passing tests

--- src/friendly_error/core.clj
+++ src/friendly_error/core.clj
@@ -1,16 +1,49 @@
 (ns friendly-error.core)
 
 (defn friendly-forms
+  "Helper function for generating forms for
+   friendly-error message macro make-friend!
+
+   Given a function and a list of per-argument
+   predicates, returns clojure forms for
+   defining a function which checks those
+   predicate as preconditions and then calls
+   the initial function.
+
+   Example:
+   (def mymap map)
+   (friendly-forms mymap fn? coll?)
+   returns the same result as:
+     (fn ([arg0 arg1]
+            {:pre [(fn? arg0) (coll? arg1)]}
+            (mymap arg0 arg1)))
+
+   If your target function has multiple
+   signatures, pass vectors of predicates
+   rather than just predicates:
+
+   (defn f1 ([a] a) ([a b] (+ a b)))
+   (friendly-forms f1 [number?]
+                      [number? number?])
+
+  Note: the forms returned include metadata
+  hooks with references to the parameters, so
+  that (un-friend! (make-friend! ...)) can
+  return the arguments passed to
+  friendly-forms."
   [fun & checklists]
-  (if (fn? (first checklists))
-    (recur fun [checklists])
+  (let [checklists
+        (if (vector? (first checklists))
+          checklists
+          [(vec checklists)])]
     `(vary-meta
       (fn
         ~@(map
            (fn [checks]
              (let [anames
-                   (map (comp symbol
-                              (partial str "arg"))
+                   (map (comp
+                         symbol
+                         (partial str "arg"))
                         (range (count checks)))
                    achecks
                    (vec (partition
@@ -23,42 +56,36 @@
            checklists))
         assoc
         :friend {:fun ~fun
-                 :checks (list ~@checklists)})))
-
+                 :checks ~(vec checklists)})))
 
 (defn friendly-fn
+  "Given arguments like those to
+   friendly-forms, returns a function with
+   the specified per-argument preconditions."
   [fun & checks]
   (eval (apply friendly-forms fun checks)))
 
 (defmacro make-friend!
+  "Replaces the current definition of fun with
+   one containing the specified per-argument
+   preconditions."
   [fun & checks]
   `(do
      (alter-var-root (var ~fun)
                      friendly-fn ~@checks)
      (var ~fun)))
 
-(defmacro un-friend! [fun]
+(defmacro un-friend!
+  "Reverses the effect of make-friend! and
+   returns a map containing enough information
+   to invoke make-friend! again."
+  [fun]
   `(when-let [friend# (:friend (meta ~fun))]
-     (alter-var-root (var ~fun)
-                     (constantly (:fun friend#)))
+     (alter-var-root
+      (var ~fun)
+      (constantly (:fun friend#)))
      friend#))
 
 ;;(friendly map is-function? is-collection?)
 ;;
-
-
-(defn q1 [a] (inc a))
-(defn q2
-  ([a]   a)
-  ([a b] (+ a b)))
-
-
-
-(clojure.core/vary-meta
- (clojure.core/fn
-  ([arg0] {:pre [(number? arg0)]} (q2 arg0))
-  ([arg0 arg1] {:pre [(number? arg0) (number? arg1)]} (q2 arg0 arg1))
-  clojure.core/assoc
-  :friend
-  {:fun q2, :checks (clojure.core/list [number?] [number? number?])}
 
--- test/friendly_error/core_test.clj
+++ test/friendly_error/core_test.clj
@@ -9,3 +9,28 @@
     (is (= [3 4] (tmap inc [2 3])))
     (is (thrown? AssertionError
                  (tmap 9 [2 3])))))
+
+(deftest single-test
+  (let [f1 (fn [a] (inc a))
+        f1-f (friendly-fn f1 number?)]
+  (testing "one param fn takes number"
+    (is (= (f1 4) (f1-f 4)))
+    (is (thrown? AssertionError
+                 (f1-f 'junk))))))
+
+(deftest multi-test
+  (let [f2 (fn
+             ([a]   a)
+             ([a b] (+ a b)))
+        f2-f (friendly-fn f2
+                          [number?]
+                          [number? number?])]
+    (testing "one/two param fn takes number or number, number"
+      (is (= (f2 4) (f2-f 4)))
+      (is (= (f2 4 5) (f2-f 4 5)))
+      (is (thrown? AssertionError
+                   (f2-f 'junk)))
+      (is (thrown? AssertionError
+                   (f2-f 'junk 5)))
+      (is (thrown? AssertionError
+                   (f2-f 4 'junk))))))
