ea982b6a30f8db277399f517b8e64ea437e2f4b4
Eric Kobrin <erluko+git@gmail.com>
2012-11-11 08:56:12 -0500

    multi-param list works

--- src/friendly_error/core.clj
+++ src/friendly_error/core.clj
@@ -1,19 +1,30 @@
 (ns friendly-error.core)
 
 (defn friendly-forms
-  [fun & checks]
-  (let [anames (map (comp symbol
-                          (partial str "arg"))
-                          (range (count checks)))
-        achecks (vec (partition
-                 2
-                 (interleave checks anames)))]
-    `(vary-meta (fn [~@anames]
-                  {:pre ~achecks}
-                  (~fun ~@anames))
-                assoc
-                :friend {:fun ~fun
-                         :checks (list ~@checks)})))
+  [fun & checklists]
+  (if (fn? (first checklists))
+    (recur fun [checklists])
+    `(vary-meta
+      (fn
+        ~@(map
+           (fn [checks]
+             (let [anames
+                   (map (comp symbol
+                              (partial str "arg"))
+                        (range (count checks)))
+                   achecks
+                   (vec (partition
+                         2
+                         (interleave
+                          checks anames)))]
+               `([~@anames]
+                   {:pre ~achecks}
+                   (~fun ~@anames))))
+           checklists))
+        assoc
+        :friend {:fun ~fun
+                 :checks (list ~@checklists)})))
+
 
 (defn friendly-fn
   [fun & checks]
@@ -34,3 +45,20 @@
 
 ;;(friendly map is-function? is-collection?)
 ;;
+
+
+(defn q1 [a] (inc a))
+(defn q2
+  ([a]   a)
+  ([a b] (+ a b)))
+
+
+
+(clojure.core/vary-meta
+ (clojure.core/fn
+  ([arg0] {:pre [(number? arg0)]} (q2 arg0))
+  ([arg0 arg1] {:pre [(number? arg0) (number? arg1)]} (q2 arg0 arg1))
+  clojure.core/assoc
+  :friend
+  {:fun q2, :checks (clojure.core/list [number?] [number? number?])}
+
