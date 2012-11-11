596bcebd9f92c4f150055e6125d98397c954a9fb
Eric Kobrin <erluko+git@gmail.com>
2012-11-11 11:57:08 -0500

    fixed project placeholder url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject friendly-error "0.1.0-SNAPSHOT"
   :description "Function wrappers with preconditions for Elena Machkasova at UMN Morris"
-  :url "http://erluko.org/clj/friendly-error"
+  :url "http://erluko.com/clj/friendly-error"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]])
